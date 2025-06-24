970b38042e0a0113539dfc93f34df7b21bad0746
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-06-24 16:25:05 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className='w-full h-screen flex flex-col items-center justify-center bg-gray-100'>
+    <div className='w-full md:h-screen h-[320px] flex flex-col items-center justify-center bg-gray-100 mt-1'>
 
-      <div className="relative w-full h-96">
-
+      <div className="relative w-full md:h-[690px] h-full mt-10 bg-[url(/imagem.jpg)] bg-cover bg-center bg-no-repeat">
+        {/* 
         <Image
           src="/imagem.jpg"
           alt="SS Contabilidade"
           fill
-          className="object-cover w-full h-full"
-        />
+          className="object-contain w-full h-full"
+        /> */}
 
       </div>
 
